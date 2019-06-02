ef0c6f7114d9f15518e70f6408785be46e773d48
tonyrud <tonrudny@gmail.com>
2019-06-02 15:43:32 -0400

    chore: remove VForm

---
+++ styleguide.config.js
@@ -0,0 +1,26 @@
+module.exports = {
+  components: 'components/**/*.vue',
+  webpackConfig: {
+    module: {
+      rules: [
+        // Vue loader
+        {
+          test: /\.vue$/,
+          exclude: /node_modules/,
+          loader: 'vue-loader',
+        },
+        // Babel loader, will use your project’s .babelrc
+        {
+          test: /\.js?$/,
+          exclude: /node_modules/,
+          loader: 'babel-loader',
+        },
+        // Other loaders that are needed for your components
+        {
+          test: /\.css$/,
+          loader: 'style-loader!css-loader',
+        },
+      ],
+    },
+  },
+}
