f349849d6dc57f176d9153d017197fbc92731a3b
tonyrud <tonrudny@gmail.com>
2019-06-02 11:53:28 -0400

    feat: test VHeadline

--- components/VHeadline/__tests__/VHeadline.test.js
+++ components/VHeadline/__tests__/VHeadline.test.js
@@ -1,67 +1,64 @@
-// import TestHelpers from '@@/.config/jest/TestHelpers'
-// import Headline from './../VHeadline'
+import Headline from './../VHeadline'
+import { shallowMount } from '@vue/test-utils'
 
-// const propsData = {
-//   upper: false,
-//   level: '2',
-//   subHeader: true,
-// }
-
-// let testHelpers
+const propsData = {
+  upper: false,
+  level: '2',
+  subHeader: true,
+}
 
 describe('Headline', () => {
-  it('runs', () => {
-    expect(true).toBe(true)
+  let wrapper
+
+  beforeEach(() => {
+    wrapper = shallowMount(Headline, {
+      propsData,
+    })
   })
-  // beforeEach(() => {
-  //   testHelpers = new TestHelpers(Headline, {
-  //     propsData,
-  //   })
-  // })
 
-  // describe('Props', () => {
-  //   it(':upper should be set to false', () => {
-  //     expect(testHelpers.wrapper.props().upper).toBeFalsy()
-  //   })
+  describe('Props', () => {
+    it(':upper should be set to false', () => {
+      expect(wrapper.props().upper).toBeFalsy()
+    })
 
-  //   it(':subHeader prop should have correct text', () => {
-  //     expect(testHelpers.wrapper.props().subHeader).toBeTruthy()
-  //   })
+    it(':subHeader prop should have correct text', () => {
+      expect(wrapper.props().subHeader).toBeTruthy()
+    })
 
-  //   it(':level prop is correct', () => {
-  //     expect(testHelpers.wrapper.props().level).toEqual('2')
-  //   })
-  // })
+    it(':level prop is correct', () => {
+      expect(wrapper.props().level).toEqual('2')
+    })
+  })
 
-  // describe('Markup and class changes', () => {
-  //   it(':upper sets correct classes', () => {
-  //     const headlineEl = testHelpers.wrapper.find('.heading__text')
-  //     expect(headlineEl.classes()).not.toContain('upper')
-  //     testHelpers.wrapper.setProps({ upper: true })
-  //     expect(headlineEl.classes()).toContain('upper')
-  //   })
+  describe('Markup and class changes', () => {
+    it(':upper sets correct classes', () => {
+      const headlineEl = wrapper.find('.heading__text')
+      expect(headlineEl.classes()).not.toContain('upper')
+      wrapper.setProps({ upper: true })
+      expect(headlineEl.classes()).toContain('upper')
+    })
 
-  //   it(':level changes classes', () => {
-  //     const headlineEl = testHelpers.wrapper.find('.heading__text')
-  //     expect(headlineEl.classes()).toContain('h2')
-  //     testHelpers.wrapper.setProps({ level: '3' })
-  //     expect(headlineEl.classes()).not.toContain('h1')
-  //     expect(headlineEl.classes()).toContain('h3')
-  //   })
-  // })
+    it(':level changes classes', () => {
+      const headlineEl = wrapper.find('.heading__text')
+      expect(headlineEl.classes()).toContain('h2')
+      wrapper.setProps({ level: '3' })
+      expect(headlineEl.classes()).not.toContain('h1')
+      expect(headlineEl.classes()).toContain('h3')
+    })
+  })
 
-  // describe('Slots', () => {
-  //   beforeEach(() => {
-  //     testHelpers = new TestHelpers(Headline, {
-  //       slots: {
-  //         default: 'slot text',
-  //       },
-  //     })
-  //   })
+  describe('Slots', () => {
+    beforeEach(() => {
+      wrapper = shallowMount(Headline, {
+        slots: {
+          default: 'slot text',
+        },
+      })
+    })
 
-  //   it('slot renders text', () => {
-  //     const slot = testHelpers.wrapper.find('.heading__text')
-  //     expect(slot.text()).toBe('slot text')
-  //   })
-  // })
+    it('slot renders text', () => {
+      const slot = wrapper.find('.heading__text')
+      expect(slot.text()).toBe('slot text')
+    })
+  })
 })
